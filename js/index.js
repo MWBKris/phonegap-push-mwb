79f1476788353583bc84b49cd0d750b6c18f86b3
MWBKris <kris@my-websitebuilder.com>
2013-07-11 11:39:19 +0200

    test

--- js/index.js
+++ js/index.js
@@ -40,40 +40,40 @@ var app = {
 
         $("#app-status-ul").append('<li>deviceready event received</li>');
                 
-        document.addEventListener("backbutton", function(e)
-        {
-            $("#app-status-ul").append('<li>backbutton event received</li>');
+        // document.addEventListener("backbutton", function(e)
+        // {
+        //     $("#app-status-ul").append('<li>backbutton event received</li>');
             
-            if( $("#home").length > 0)
-            {
-                // call this to get a new token each time. don't call it to reuse existing token.
-                //pushNotification.unregister(successHandler, errorHandler);
-                e.preventDefault();
-                navigator.app.exitApp();
-            }
-            else
-            {
-                navigator.app.backHistory();
-            }
-        }, false);
+        //     if( $("#home").length > 0)
+        //     {
+        //         // call this to get a new token each time. don't call it to reuse existing token.
+        //         //pushNotification.unregister(successHandler, errorHandler);
+        //         e.preventDefault();
+        //         navigator.app.exitApp();
+        //     }
+        //     else
+        //     {
+        //         navigator.app.backHistory();
+        //     }
+        // }, false);
 
-        try 
-        { 
-            pushNotification = window.plugins.pushNotification;
-            if (device.platform == 'android' || device.platform == 'Android') {
-                $("#app-status-ul").append('<li>registering android</li>');
-                pushNotification.register(successHandler, errorHandler, {"senderID":"395880463247","ecb":"onNotificationGCM"});     // required!
-            } else {
-                $("#app-status-ul").append('<li>registering iOS</li>');
-                pushNotification.register(tokenHandler, errorHandler, {"badge":"true","sound":"true","alert":"true","ecb":"onNotificationAPN"});    // required!
-            }
-        }
-        catch(err) 
-        { 
-            txt="There was an error on this page.\n\n"; 
-            txt+="Error description: " + err.message + "\n\n"; 
-            alert(txt); 
-                } 
+        // try 
+        // { 
+        //     pushNotification = window.plugins.pushNotification;
+        //     if (device.platform == 'android' || device.platform == 'Android') {
+        //         $("#app-status-ul").append('<li>registering android</li>');
+        //         pushNotification.register(successHandler, errorHandler, {"senderID":"395880463247","ecb":"onNotificationGCM"});     // required!
+        //     } else {
+        //         $("#app-status-ul").append('<li>registering iOS</li>');
+        //         pushNotification.register(tokenHandler, errorHandler, {"badge":"true","sound":"true","alert":"true","ecb":"onNotificationAPN"});    // required!
+        //     }
+        // }
+        // catch(err) 
+        // { 
+        //     txt="There was an error on this page.\n\n"; 
+        //     txt+="Error description: " + err.message + "\n\n"; 
+        //     alert(txt); 
+        //         } 
 
        // app.receivedEvent('deviceready');
     },
