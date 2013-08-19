3a76526312bed64dc81582806a947542b21ef7f2
MWBKris <kris@my-websitebuilder.com>
2013-08-19 17:05:28 +0200

    Test

--- js/index.js
+++ js/index.js
@@ -80,9 +80,39 @@ var app = {
 
     },
 	
-	successHandler: function(){
-		alert('succes');
+	successHandler: function(result) {
+		alert('Callback Success! Result = '+result)
 	},
+	
+	errorHandler:function(error) {
+		alert(error);
+	},	
+	
+	onNotificationGCM: function(e) {
+        switch( e.event )
+        {
+            case 'registered':
+                if ( e.regid.length > 0 )
+                {
+                    console.log("Regid " + e.regid);
+                    alert('registration id = '+e.regid);
+                }
+            break;
+ 
+            case 'message':
+              // this is the actual push notification. its format depends on the data model from the push server
+              alert('message = '+e.message+' msgcnt = '+e.msgcnt);
+            break;
+ 
+            case 'error':
+              alert('GCM error = '+e.msg);
+            break;
+ 
+            default:
+              alert('An unknown GCM event has occurred');
+              break;
+        }
+    },
 
     // Update DOM on a Received Event
     receivedEvent: function(id) {
